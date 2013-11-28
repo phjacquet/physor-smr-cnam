106c8c963fe629ca3792af2442636d6ac6f2a3c9
pjacquet.physor@gmail.com <pjacquet.physor@gmail.com@31a21b3c-d74b-dffe-fac0-1d2f8f923c1e>
2013-11-28 16:41:08 +0000

    cours4 - cr

---
+++ trunk/cours4/Karcouchegilles/data/rep2D.bore_p10.d
@@ -0,0 +1,89 @@
+LINKED_LIST
+  LIBMIX   LIBMIXS
+  GCELL    
+  TRACK SYS FLUX
+  EDC
+  DATABASE DBC
+  ;
+MODULE
+  GEO: LIB: 
+  SHI: USS: 
+  SYBILT: EXCELT: SNT: ASM: FLU: EDI:
+  DELETE: END:
+  PSP: UTL: COMPO: EVO:
+  ;
+*----
+*  Microscopic cross sections from JEF2 draglib file
+*----
+LIBMIX := LIB: :: EDIT 0
+     NMIX 2
+     CTRA WIMS
+     SUBG
+     ANIS 2
+     DEPL LIB: DRAGON FIL: DLIB_J2_361
+     MIXS LIB: DRAGON FIL: DLIB_J2_361
+
+MIX 1 300.0
+ O16 = 'O16' 2.8269E-02
+ U235 = 'U235' 2.6854E-04 1
+ U238 = 'U238' 7.4039E-03 1
+ Zr91 = 'Zr91' 4.2223E-03
+ H1H2O = 'H1_H2O' 2.5848E-02
+ B10 = 'B10' 4.23E-08
+ B11 = 'B11' 1.69-07
+ 
+MIX 2 300.0
+ B10 = 'B10' 4.2681E-03
+ B11 = 'B11' 1.7180E-02
+ C = 'C0' 5.3557E-03
+ H1H2O = 'H1_H2O' 3.4386E-02
+ O16H2O = 'O16' 1.7193E-02
+;
+
+GCELL := GEO: ::  TUBEZ 4 6
+  R+ VOID  Z- VOID Z+ VOID 
+  RADIUS 0.0 5.0 65. 70. 120.
+  MESHZ   
+  0.0        
+  10.0
+  20.0
+  30.0
+  40.0
+  50.0
+  60.0
+
+  MIX 
+  2 1 2 1 
+  2 1 2 1 
+  2 1 2 1 
+  1 1 1 1 
+  1 1 1 1 
+  1 1 1 1 
+  SPLITR 1 4 1 4
+ ;
+*******************************************
+
+
+TRACK := SNT: GCELL ::
+     EDIT 0
+     MAXR 1000
+     DIAM 1 SN 8
+     SCAT 2
+     QUAB 7
+     TITLE 'CORE'
+     ;
+
+LIBMIXS := USS: LIBMIX TRACK :: EDIT 0 
+GRMAX 1 ARM ;
+
+SYS := ASM: LIBMIXS TRACK :: EDIT 0 ARM ;
+
+FLUX := FLU: SYS LIBMIXS  TRACK  ::
+     EDIT 1
+     TYPE K
+     EXTE 100 1E-4 THER 10 1E-4
+     ;
+
+
+
+END: ;
---
+++ trunk/cours4/akim/data/spx2D.G20.d
@@ -0,0 +1,94 @@
+LINKED_LIST
+  LIBMIX   LIBMIXS
+  GCELL    
+  TRACK SYS FLUX
+  EDC
+  DATABASE DBC
+  ;
+MODULE
+  GEO: LIB: 
+  SHI: USS: 
+  SYBILT: EXCELT: SNT: ASM: FLU: EDI:
+  DELETE: END:
+  PSP: UTL: COMPO: EVO:
+  ;
+*----
+*  Microscopic cross sections from JEF2 draglib file
+*----
+LIBMIX := LIB: :: EDIT 0
+     NMIX 2
+     CTRA WIMS
+     SUBG
+     ANIS 2
+     DEPL LIB: DRAGON FIL: DLIB_J2_361
+     MIXS LIB: DRAGON FIL: DLIB_J2_361
+
+MIX 1 300.0
+Pu239 = 'Pu239' 1.3090E-03 1
+U238 = 'U238' 8.9011E-03 1
+O16 = 'O16' 2.0420E-02
+Na23 = 'Na23' 6.6557E-03
+Fe56 = 'Fe56' 9.3451E-03
+
+MIX 2 300.0
+ B10 = 'B10' 8.1571E-03
+ B11 = 'B11' 3.2833E-02
+ C = 'C0' 1.0236E-02
+ O16H2O = 'O16' 1.7193E-02
+ Na23 = 'Na23' 2.2430E-02
+;
+
+GCELL := GEO: ::  TUBEZ 4 9
+  R+ VOID  Z- VOID Z+ VOID 
+  RADIUS 0.0 5.0 70. 75. 148.
+  MESHZ   
+  0.0        
+  10.0
+  20.0
+  30.0
+  40.0
+  50.0
+  60.0
+  70.0
+  80.0
+  90.0
+
+  MIX 
+  2 1 2 1 
+  2 1 2 1 
+  1 1 2 1 
+  1 1 2 1 
+  1 1 2 1 
+  1 1 1 1
+  1 1 1 1 
+  1 1 1 1 
+  1 1 1 1
+
+  SPLITR 1 4 1 4
+ ;
+*******************************************
+
+
+TRACK := SNT: GCELL ::
+     EDIT 0
+     MAXR 1000
+     DIAM 1 SN 8
+     SCAT 2
+     QUAB 7
+     TITLE 'CORE'
+     ;
+
+LIBMIXS := USS: LIBMIX TRACK :: EDIT 0 
+GRMAX 1 ARM ;
+
+SYS := ASM: LIBMIXS TRACK :: EDIT 0 ARM ;
+
+FLUX := FLU: SYS LIBMIXS  TRACK  ::
+     EDIT 1
+     TYPE K
+     EXTE 100 1E-4 THER 10 1E-4
+     ;
+
+
+
+END: ;
